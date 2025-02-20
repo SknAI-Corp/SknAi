23cd2dcfbcb5d8d733bb2553ba9f174adb319d3c
imsky003 <53104747+imsky003@users.noreply.github.com>
2025-02-20 15:14:59 -0500

    Update App.js

--- Frontend/sknai-classifier/src/App.js
+++ Frontend/sknai-classifier/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { Container, Button, Typography, CircularProgress } from "@mui/material";
+import { Container, Button, Typography, CircularProgress, AppBar, Toolbar, Card, CardContent, Box  } from "@mui/material";
 
 const App = () => {
     const [selectedImage, setSelectedImage] = useState(null);
@@ -35,7 +35,13 @@ const App = () => {
     };
 
     return (
-        <Container maxWidth="sm" style={{ marginTop: "50px", textAlign: "center" }}>
+        <>
+        <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6">Image Classifier</Typography>
+                </Toolbar>
+         </AppBar>
+        {/* <Container maxWidth="sm" style={{ marginTop: "50px", textAlign: "center" }}>
             <Typography variant="h4">Chat Image Classifier</Typography>
             <input type="file" onChange={handleImageUpload} />
             <Button variant="contained" color="primary" onClick={handleSubmit} style={{ marginTop: "20px" }}>
@@ -43,7 +49,36 @@ const App = () => {
             </Button>
             {loading && <CircularProgress style={{ marginTop: "20px" }} />}
             {prediction && <Typography variant="h6" style={{ marginTop: "20px" }}>Prediction: {prediction}</Typography>}
-        </Container>
+        </Container> */}
+        <Card sx={{ maxWidth: 400, textAlign: "center", padding: 2, margin: "auto", mt:"80px" }}>
+      <CardContent>
+        <Typography variant="h6" gutterBottom>
+          Upload an Image
+        </Typography>
+        <Box sx={{ marginBottom: 2, marginTop: 6 }}>
+          {/* <input
+            type="file"
+            
+            onChange={handleImageUpload}
+            style={{ display: "none" }}
+            
+          />
+          <label htmlFor="upload-button">
+            <Button variant="contained" onClick={handleSubmit} component="span">
+              Choose File
+            </Button>
+          </label> */}
+          <input type="file" onChange={handleImageUpload} />
+            <Button variant="contained" color="primary" onClick={handleSubmit} style={{ marginTop: "20px" }}>
+                Predict
+            </Button>
+        </Box>
+        
+      </CardContent>
+      {loading && <CircularProgress style={{ marginTop: "20px" }} />}
+            {prediction && <Typography variant="h6" style={{ marginTop: "20px" }}>Prediction: {prediction}</Typography>}
+    </Card>
+        </>
     );
 };
 
