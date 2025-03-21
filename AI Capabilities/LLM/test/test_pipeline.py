70c75042134440aaaf3a829da0b35b0257519b3d
Jainam Patel <jainampatel68@gmail.com>
2025-03-21 14:48:00 -0400

    Made test pipeline

--- AI Capabilities/LLM/test/test_pipeline.py
+++ AI Capabilities/LLM/test/test_pipeline.py
@@ -6,7 +6,7 @@ def test_pipeline():
     pytest.main(["test_embedding.py"])
     
     # Run the second test file
-    pytest.main(["test_llm_api_test.py"])
+    pytest.main(["llm_api_test.py"])
 
 if __name__ == "__main__":
     test_pipeline()
